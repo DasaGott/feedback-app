4efe02df4c847641cd8cbcfa2cc1e75d168a5489
DasaGott <142871686+DasaGott@users.noreply.github.com>
2024-01-26 11:31:46 +0100

    Added real-time validation to the input

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -4,8 +4,27 @@ import Button from "./shared/Button";
 
 function FeedbackForm() {
   const [text, setText] = useState("");
+  const [btnDisabled, setBtnDisabled] =
+    useState(true);
+  const [message, setMessage] = useState("");
 
   const handleTextInput = (event) => {
+    if (text === "") {
+      setBtnDisabled(true);
+      setMessage(null);
+    } else if (
+      text != "" &&
+      text.trim().length < 10
+    ) {
+      setBtnDisabled(true);
+      setMessage(
+        "Text has to be at least 10 characters long"
+      );
+    } else {
+      setBtnDisabled(false);
+      setMessage(null);
+    }
+
     setText(event.target.value);
   };
 
@@ -21,8 +40,16 @@ function FeedbackForm() {
             placeholder="Write a review"
             value={text}
           ></input>
-          <Button type="submit">Send</Button>
+          <Button
+            type="submit"
+            isDisabled={btnDisabled}
+          >
+            Send
+          </Button>
         </div>
+        {message && (
+          <div className="message">{message}</div>
+        )}
       </form>
     </Card>
   );
